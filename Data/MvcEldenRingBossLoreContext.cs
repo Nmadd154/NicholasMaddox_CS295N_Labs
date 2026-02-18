c0ad02e38aa220acdf3fd4c221ab7362ce648761
Nicholas Maddox <nmadd154@gmail.com>
2026-02-17 18:32:18 -0800

    Edited code to use MySql

--- Data/MvcEldenRingBossLoreContext.cs
+++ Data/MvcEldenRingBossLoreContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MvcEldenRingBossLore.Models;
@@ -10,10 +11,7 @@ namespace MvcEldenRingBossLore.Data
 {
     public class MvcEldenRingBossLoreContext : DbContext
     {
-        public MvcEldenRingBossLoreContext (DbContextOptions<MvcEldenRingBossLoreContext> options)
-            : base(options)
-        {
-        }
+        public MvcEldenRingBossLoreContext (DbContextOptions<MvcEldenRingBossLoreContext> options): base(options){ }
 
         public DbSet<MvcEldenRingBossLore.Models.Lore> Lore { get; set; } = default!;
         public DbSet<MvcEldenRingBossLore.Models.Posts> BlogPosts { get; set; } = default!;
