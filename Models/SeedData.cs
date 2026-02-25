5dc5b91ccdb292d6bf826d79a1b22de7c723ef4a
Nicholas Maddox <nmadd154@gmail.com>
2026-02-24 20:09:44 -0800

    Added Seed Data and initialization

---
+++ Models/SeedData.cs
@@ -0,0 +1,54 @@
+// Created by Nicholas Maddox
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcEldenRingBossLore.Data;
+using System;
+using System.Linq;
+
+namespace MvcEldenRingBossLore.Models;
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcEldenRingBossLoreContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcEldenRingBossLoreContext>>()))
+        {
+            if (context.Lore.Any())
+            {
+                return;
+            }
+            context.Lore.AddRange(
+                new Lore
+                {
+                    Title = "Margit, the Fell Omen",
+                    DiscoveryDate = DateTime.Parse("2022-2-25"),
+                    GodType = "Demigod",
+                    Notes = "A powerful Omen who guards Stormveil Castle"
+                },
+                new Lore
+                {
+                    Title = "Godrick the Grafted",
+                    DiscoveryDate = DateTime.Parse("2022-2-25"),
+                    GodType = "Demigod",
+                    Notes = "Lord of Stormveil Castle who grafts limbs of defeated foes"
+                },
+                new Lore
+                {
+                    Title = "Radahn, Starscourge",
+                    DiscoveryDate = DateTime.Parse("2022-2-25"),
+                    GodType = "Demigod",
+                    Notes = "Legendary warrior who conquered the stars"
+                },
+                new Lore
+                {
+                    Title = "Malenia, Blade of Miquella",
+                    DiscoveryDate = DateTime.Parse("2022-2-25"),
+                    GodType = "Demigod",
+                    Notes = "Undefeated swordswoman cursed with Scarlet Rot"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,6 +21,12 @@ builder.Services.AddDbContext<MvcEldenRingBossLoreContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
